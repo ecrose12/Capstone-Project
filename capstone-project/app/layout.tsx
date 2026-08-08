fd9d2debf594fc08f27339728b6d08160f52a4ae
Laura Sohl <laura.m.sohl@gmail.com>
2026-08-08 12:48:34 -0500

    Some styling, I think

--- capstone-project/app/layout.tsx
+++ capstone-project/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Lexend } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -9,6 +9,10 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+const lexend = Lexend({
+  variable: "--font-lexend",
   subsets: ["latin"],
 });
 
